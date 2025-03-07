5f50e837f36f48f629688aa29b0dc3987abe941a
AndressaARodrigues <andressa.rodruigues33@gmail.com>
2025-03-06 22:39:07 -0300

    Interface Segregation Principle

--- src/srp/main.ts
+++ src/srp/main.ts
@@ -12,12 +12,19 @@ Mais simples: Subtipos precisam ser substituíveis por seus tipos de base.
 Mais simples ainda: Se meu programa espera um Animal, algo do tipo
 Cachorro (que herda de Animal) deve servir como qualquer outro Animal.
 */
+
+/*
+Interface segregation principle (Princípio da segregação de Interface) -
+os clientes não devem ser forçados a depender de types, interfaces ou membros
+abstratos que não utilizam
+*/
 import { ShoppingCart } from './classes/shopping-cart';
 import { Order } from './classes/order';
 import { Messaging } from './services/messaging';
 import { Persistency } from './services/persistency';
 import { Product } from './classes/product';
 import { NoDiscount } from './classes/discount';
+import { EnterpriseCustomer } from './classes/customer';
 
 // const fiftyPercentDiscount = new FiftyPercentDiscount();
 // const tenPercentDiscount = new TenPercentDiscount();
@@ -25,7 +32,21 @@ const noDiscount = new NoDiscount();
 const shoppingCart = new ShoppingCart(noDiscount);
 const messaging = new Messaging();
 const persistency = new Persistency();
-const order = new Order(shoppingCart, messaging, persistency);
+// const individualCustomer = new IndividualCustomer(
+//   'Luiz',
+//   'Miranda',
+//   '111.111.111-11',
+// );
+const enterpriseCustomer = new EnterpriseCustomer(
+  'Empresa Gigante',
+  '2222222222222222',
+);
+const order = new Order(
+  shoppingCart,
+  messaging,
+  persistency,
+  enterpriseCustomer,
+);
 
 shoppingCart.addItem(new Product('Camiseta', 49.91));
 shoppingCart.addItem(new Product('Caderno', 9.9123));
