453b1b122413fed21153c2f8fcdeb2822445e100
AndressaARodrigues <andressa.rodruigues33@gmail.com>
2025-03-06 22:34:04 -0300

    Liskov substitution principle

--- src/srp/main.ts
+++ src/srp/main.ts
@@ -2,17 +2,27 @@
 Open/closed principle
 Entidades devem estar abertas para extensão, mas fechadas para modificação.
 */
+
+/*
+Liskov substitution principle (Princípio da substituição de Liskov) -
+Se ϕ(x) é uma propriedade demonstrável dos objetos x de tipo T. Então ϕ(y)
+deve ser verdadeiro para objetos y de tipo S onde S é um subtipo de T.
+
+Mais simples: Subtipos precisam ser substituíveis por seus tipos de base.
+Mais simples ainda: Se meu programa espera um Animal, algo do tipo
+Cachorro (que herda de Animal) deve servir como qualquer outro Animal.
+*/
 import { ShoppingCart } from './classes/shopping-cart';
 import { Order } from './classes/order';
 import { Messaging } from './services/messaging';
 import { Persistency } from './services/persistency';
 import { Product } from './classes/product';
-import { FiftyPercentDiscount } from './classes/discount';
+import { NoDiscount } from './classes/discount';
 
-const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
 // const tenPercentDiscount = new TenPercentDiscount();
-// const noDiscount = new NoDiscount();
-const shoppingCart = new ShoppingCart(fiftyPercentDiscount);
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
 const messaging = new Messaging();
 const persistency = new Persistency();
 const order = new Order(shoppingCart, messaging, persistency);
